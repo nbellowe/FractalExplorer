96f3bf29cd1a0e341f3c3774dc70fab15a1e4996
Nathan Bellowe <nbellowe@gmail.com>
2015-12-15 18:19:50 -0700

    Remove app

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,14 @@ module.exports = function(grunt) {
         src:  'fractal.js',
         dest: 'FractalExplorer/www/js/fractal.js'
       },
+      threeJsDef:{
+          files: [{expand: true, flatten: true, src: ['../../DefinitelyTyped/threejs/**/*'], dest: '../libs/three/', filter: 'isFile'}]
+      },
+      moveLib:{
+          files: [{expand: true, flatten: true, src: '../libs/**/*', dest:'FractalExplorer/www/js/', filter: 'isFile'}]
+      }
     },
   });
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.registerTask('default', ['copy:main']);
+  grunt.registerTask('default', ['copy:main', 'copy:threeJsDef', 'copy:moveLib']);
 };
